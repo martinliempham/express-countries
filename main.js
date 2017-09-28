fc2dba3d11abfc6a8a7e25d47775c2bbca9e1128
Martin Pham <martin.liem.pham@gmail.com>
2017-09-28 14:17:48 -0400

    final push app works

--- main.js
+++ main.js
@@ -1,44 +1,27 @@
 var fs = require('fs');
-var myArgs = process.argv[2];
- // console.log('myArgs: ', myArgs);
+
 var express = require('express');
 var app = express();
 
-app.use(express.static('express-countries'));
-
-app.get('/countries/:code', function(request, response) {
-  console.log('got request for "/hello/world"');    
-  response.send('hello there!');
-  response.render('index')
-});
-
-//for pug
-// app.set('view engine','pug');
-
-// look for templates in my views folder otherwise look for templates in views folder
-// app.set('views','./my-views');
-// '/'  is the default
+app.set('view engine','pug');
+app.set('views','./my-views');
 
 
+app.get('/:code', function(request, response) {
+	var userId=request.params.code.toUpperCase();
 
-fs.readFile('./countries.json', 'utf-8', function(err, countries){
-	if(err) {
-		console.log(`an error occurred: ${err}`);
-		throw err;
-	}
-	var parser = require('body-parser');
-	app.use(parser.json(countries));
-
-	var countries = JSON.parse(countries);
-
-	for (var i = 0; i < countries.length; i++) {
-		var i = countries[i]
-		if(myArgs === countries[i].name){
-			console.log(countries[i].name, countries[i].nativeName, countries[i].code, countries[i].population, countries[i].region, countries[i].flag)
+	fs.readFile('./countries.json', 'utf-8', function(err, jsonFile){
+		if(err) {
+			console.log(`an error occurred: ${err}`);
+			throw err;
 		}
-	}
 
-})
+		var countries = JSON.parse(jsonFile);
+		var countriesParsed = countries[userId];
+		  response.render('list',countriesParsed)
+		
+	});
+});
 
 app.listen(3000, function(){
   console.log('App is listening on port 3000');
